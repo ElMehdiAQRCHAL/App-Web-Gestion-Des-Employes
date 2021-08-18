6f523f4a719caead2eada22e81c9408d4d50f348
ElMehdiAQRCHAL <76263226+ElMehdiAQRCHAL@users.noreply.github.com>
2021-08-18 17:40:54 +0100

    Update templet_login.php

--- View/templet_login.php
+++ View/templet_login.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link href="img/icon.png" rel="icon">
-    <title>S'identifier</title>
+    <title>S identifier</title>
 
-   
+ 
 ?>
