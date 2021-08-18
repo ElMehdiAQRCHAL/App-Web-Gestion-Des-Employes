dc2a9fd40b1dcee02c6e6fd1c4db3b747d6fe405
ElMehdiAQRCHAL <76263226+ElMehdiAQRCHAL@users.noreply.github.com>
2021-08-18 17:39:50 +0100

    Update templet_login.php

--- View/templet_login.php
+++ View/templet_login.php
@@ -6,47 +6,5 @@
 	<link href="img/icon.png" rel="icon">
     <title>S'identifier</title>
 
-    <!-- Font Icon -->
-    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
-
-    <!-- Main css -->
-    <link rel="stylesheet" href="css/style_login.css">
-</head>
-<body>
-        <!-- Sing in  Form -->
-        <section class="sign-in">
-            <div class="container">
-                <div class="signin-content">
-                    <div class="signin-image">
-                        <figure><img src="img/signin-image.jpg" alt="sing up image"></figure>
-                        <a href="index.php?action=templet_register" class="signup-image-link">Créer un compte</a>
-                    </div>
-
-                    <div class="signin-form">
-                        <img src="img/onepservices.png" style="margin-bottom:60px;">
-                        <form method="POST" class="register-form" id="login-form" action="index.php?action=VerifierLogin" >
-                            <div class="form-group">
-                                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="username"  placeholder="Nom d'utilisateur" required/>
-                            </div>
-                            <div class="form-group">
-                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="pwd"  placeholder="mot de passe" required/>
-                            </div>
-                            <div class="form-group">
-                                <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
-                                <label for="remember-me" class="label-agree-term"><span><span></span></span>souviens-toi de moi</label>
-                            </div>
-                            <div class="form-group form-button">
-                                <input type="submit" name="signin" id="signin" class="form-submit" value="S'dentifier"/>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
-    <!-- JS -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="js/main.js"></script>
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+   
 ?>
